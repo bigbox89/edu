02f1c3d4c90baec9e52a59fb2dde036f16cf5e3b
Petr Pyatkov <Bigbox89@mail.ru>
2023-12-26 13:49:21 +0500

    Попраавил работу с контекстом Поправилработу с дефолтным профилем

--- Lesson1/src/main/java/com/petr/app/AppApplication.java
+++ Lesson1/src/main/java/com/petr/app/AppApplication.java
@@ -1,50 +1,49 @@
 package com.petr.app;
 
-import com.petr.app.config.DefaultAppConfig;
+import com.petr.app.config.AppConfig;
 import com.petr.app.service.ContactService;
 import com.petr.app.service.ProfileWorker;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.Scanner;
+
 public class AppApplication {
+    public static void main(String[] args) {
+        try (AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class)) {
+            ProfileWorker profileWorker = applicationContext.getBean(ProfileWorker.class);
+            ContactService contactService = new ContactService(profileWorker);
+            Scanner scanner = new Scanner(System.in);
+            while (true) {
+                System.out.println("Enter command (displayContacts, addContact, deleteContactByEmail, saveContactsToFile):");
+                String command = scanner.nextLine();
 
-	static ApplicationContext applicationContext = new AnnotationConfigApplicationContext(DefaultAppConfig.class);
-
-	public static void main(String[] args) {
-		ProfileWorker profileWorker = applicationContext.getBean(ProfileWorker.class);
-		ContactService contactService = new ContactService(profileWorker);
-		Scanner scanner = new Scanner(System.in);
-		while (true) {
-			System.out.println("Enter command (displayContacts, addContact, deleteContactByEmail, saveContactsToFile):");
-			String command = scanner.nextLine();
-
-			switch (command) {
-				case "displayContacts":
-					contactService.displayContacts();
-					break;
-				case "addContact":
-					System.out.println("Enter the contact details in the format: Surname, First name, Patronymic;Phone number;Email");
-					String inputContact = scanner.nextLine();
-					String[] inputContactArray = inputContact.split(";");
-					String addContactResult = contactService.addContact(inputContactArray);
-					System.out.println(addContactResult);
-					break;
-				case "deleteContactByEmail":
-					System.out.println("Enter the Email address of the contact you want to delete:");
-					String emailToDelete = scanner.nextLine();
-					String deleteContactResult = contactService.deleteContactByEmail(emailToDelete);
-					System.out.println(deleteContactResult);
-					break;
-				case "saveContactsToFile":
-					String saveContactsResult = contactService.saveContactsToFile();
-					System.out.println(saveContactsResult);
-					break;
-				default:
-					System.out.println("Incorrect command. Try again.");
-					break;
-			}
-			System.out.println();
-		}
-	}
+                switch (command) {
+                    case "displayContacts":
+                        contactService.displayContacts();
+                        break;
+                    case "addContact":
+                        System.out.println("Enter the contact details in the format: Surname, First name, Patronymic;Phone number;Email");
+                        String inputContact = scanner.nextLine();
+                        String[] inputContactArray = inputContact.split(";");
+                        String addContactResult = contactService.addContact(inputContactArray);
+                        System.out.println(addContactResult);
+                        break;
+                    case "deleteContactByEmail":
+                        System.out.println("Enter the Email address of the contact you want to delete:");
+                        String emailToDelete = scanner.nextLine();
+                        String deleteContactResult = contactService.deleteContactByEmail(emailToDelete);
+                        System.out.println(deleteContactResult);
+                        break;
+                    case "saveContactsToFile":
+                        String saveContactsResult = contactService.saveContactsToFile();
+                        System.out.println(saveContactsResult);
+                        break;
+                    default:
+                        System.out.println("Incorrect command. Try again.");
+                        break;
+                }
+                System.out.println();
+            }
+        }
+    }
 }
--- Lesson1/src/main/java/com/petr/app/config/DefaultAppConfig.java
+++ Lesson1/src/main/java/com/petr/app/config/DefaultAppConfig.java
@@ -1,11 +1,16 @@
 package com.petr.app.config;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+import com.petr.app.service.FileService;
+import com.petr.app.service.impl.TestFileService;
+import org.springframework.context.annotation.*;
 
 @ComponentScan("com.petr.app")
 @Configuration
 @PropertySource("classpath:application.properties")
+@Profile("default")
 public class DefaultAppConfig {
+    @Bean
+    public FileService fileService() {
+        return new TestFileService();
+    }
 }
