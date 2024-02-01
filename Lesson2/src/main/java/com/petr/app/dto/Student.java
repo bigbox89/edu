0c4c7d98ac30bfdf31ce5762f00506f2cd06f1ee
Petr Pyatkov <Bigbox89@mail.ru>
2024-02-01 17:49:50 +0500

    Lesson2 Добавил @Data убрал field injection

--- Lesson2/src/main/java/com/petr/app/dto/Student.java
+++ Lesson2/src/main/java/com/petr/app/dto/Student.java
@@ -1,7 +1,7 @@
 package com.petr.app.dto;
 
 import lombok.*;
-
+@NoArgsConstructor
 @Data
 public class Student {
     @NonNull
@@ -12,50 +12,4 @@ public class Student {
     private String lastName;
     @NonNull
     private int age;
-
-    public Student() {
-
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "Student{" +
-                "id=" + id +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", age=" + age +
-                '}';
-    }
 }
--- Lesson2/src/main/java/com/petr/app/service/impl/StudentCommandsImpl.java
+++ Lesson2/src/main/java/com/petr/app/service/impl/StudentCommandsImpl.java
@@ -3,14 +3,16 @@ package com.petr.app.service.impl;
 import com.petr.app.dto.Student;
 import com.petr.app.service.StudentCommands;
 import com.petr.app.service.StudentService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 
 @ShellComponent
 public class StudentCommandsImpl implements StudentCommands {
-    @Autowired
-    private StudentService studentService;
+    private final StudentService studentService;
+
+    public StudentCommandsImpl(StudentService studentService) {
+        this.studentService = studentService;
+    }
 
     @ShellMethod("Add a new student")
     public void addStudent(String firstName, String lastName, int age) {
